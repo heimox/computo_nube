be7a7a34ef5bcd38eaa54b1be1dd81306baff0ba
heimox <heimox115@gmail.com>
2024-06-21 11:18:04 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const http = require('http');
 
 const port = 3000;
-const hostname = 52.23.204.79;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -10,5 +9,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at port ${port}/`);
 });
