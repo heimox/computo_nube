5ce84f6c74754e66f2143d092799e1173e06a2c0
heimox <heimox115@gmail.com>
2024-07-02 03:56:39 -0600

    Create azure.js

---
+++ azure.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hola Mundo\n');
+});
+
+server.listen(22, () => {
+  console.log('Server running at port 22/');
+});
